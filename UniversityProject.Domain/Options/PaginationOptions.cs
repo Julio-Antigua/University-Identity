9045f8cc56439abe3562e08ed3e4362d7e2fb575
Julio-Antigua <juliosaul12@gmail.com>
2022-08-08 12:13:50 -0400

    -- Corregimos el nombre de una variable

--- UniversityProject.Domain/Options/PaginationOptions.cs
+++ UniversityProject.Domain/Options/PaginationOptions.cs
@@ -3,6 +3,6 @@
     public class PaginationOptions
     {
         public int DefaultPageSize { get; set; }
-        public int DefaulPageNumber { get; set; }
+        public int DefaultPageNumber { get; set; }
     }
 }
