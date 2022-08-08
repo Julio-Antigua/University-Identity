acbaa5f5557d9981a994b60141e316407ed8a58c
Julio-Antigua <juliosaul12@gmail.com>
2022-08-08 12:10:34 -0400

    -- Agregamos una clase helper para la estadarizacion de algunas variables

---
+++ UniversityProject.Domain/Enumerations/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityProject.Domain.Enumerations
+{
+    public class Helpers
+    {
+        public static string xmlFile => $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        public static string Uri => "/api/Student";
+    }
+}
