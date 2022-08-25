47056bb75e7129f8449c2f10115f38632140e4ca
Julio-Antigua <juliosaul12@gmail.com>
2022-08-25 14:56:22 -0400

    -- Agrege la clase Page en la carpeta Helpers

---
+++ UniversityProject.Domain/Helpers/Help.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace UniversityProject.Domain.Helpers
+{
+    public class Help
+    {
+        public static string xmlFile => $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        public static string Uri => "/api/Student";
+    }
+}
---
+++ UniversityProject.Domain/Helpers/Page.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+/// <summary>
+/// Representa una página de una entidad determinada con una lista de elementos de dicha entidad.
+/// </summary>
+/// <typeparam name="TEntity">entidad correspondiente a la página.</typeparam>
+public class Page<TEntity>
+    {
+        /// <summary>
+        /// Obtiene o establece el número de página actual.
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el total de elementos en la base de datos correspondiente a esta entidad.
+        /// </summary>
+        public int TotalItems { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece el total de páginas de acuerdo a la cantidad de elementos en la base de datos.
+        /// </summary>
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// Obtiene o establece los valores resumidos correspondientes.
+        /// </summary>
+        public List<KeyValuePair<object, object>> Summary { get; set; } = new List<KeyValuePair<object, object>>();
+
+        /// <summary>
+        /// Obtiene o establece los ítems correspondientes a esta página.
+        /// </summary>
+        public IList<TEntity> Items { get; set; }
+    }
